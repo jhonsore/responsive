05c2576fb387bce4b33433b93d19b676c9661bd2
jhonsore <jhonnatansr@gmail.com>
2015-05-10 14:39:38 -0300

    Projeto inicializado

---
+++ exemplos/responsive-images.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Responsive</title>
+
+    <!-- Bootstrap core CSS -->
+	<style type="text/css" media="all">  @import url(../css/reset.css);  @import url(../css/generic.css);  </style> 
+	<style type="text/css" media="print"> @import url(../css/print.css) print; </style>
+    <!-- Custom Google Web Font -->
+    <!-- Add custom CSS here -->
+    <link href="../css/responsive.css" rel="stylesheet">
+    <style type="text/css">
+    	#menu{ height:50px; border-bottom:1px solid #999;}
+		#menu h1{ float:left; margin:18px 0 0 20px; font-size:18px;}
+		#menu ul{ float:right; margin-top:20px;}
+		#menu ul li{ margin:0px 10px; float:left; font-size:13px;}
+		
+		.center{ width:980px; margin:40px auto; display:block;}
+		
+		.POMEGRANATE{ background-color:#F22613;}
+		.FLAMINGO{ background-color:#EF4836;}
+		.TALL{ background-color:#C0392B;}
+		.CABARET{ background-color:#D2527F;}
+		.WAX { background-color:#F1A9A0;}
+		.REBECCAPURPLE{ background-color:#663399;}
+		.HONEY { background-color:#674172;}
+		.MEDIUM { background-color:#BF55EC;}
+		.MARINO{ background-color:#446CB3;}
+		.EBONY { background-color:#22313F;}
+		.EUCALYPTUS{ background-color:#26A65B;}
+		.GOLD{ background-color:#E87E04;}
+		.CASABLANCA{ background-color:#F4B350;}
+				
+		.resp-box > div{ padding:10px;}
+		
+		@media (max-width: 980px){
+			.center{ width:auto;}
+		}
+
+    </style>
+</head>
+
+<body>
+	<div id="menu">
+    	<h1>BRAND</h1>
+        <ul>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+        </ul>
+        <div class="clear"></div>
+    </div>
+    <div class="center">
+        <div>
+            <div class="resp-box col-1-2 col-sm-1"><div><img src="img/benefit800x800.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-1-2 col-sm-1"><div><img src="img/jaqk800x800.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-1-3"><div><img src="img/casal_de_bico_de_pimenta.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-1-3"><div><img src="img/tiesangue_zps48109dd1.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-1-3"><div><img src="img/PSSARO~1.JPG" class="img-responsive"></div></div>
+            <div class="resp-box col-1-2 col-sm-1-2"><div><img src="img/seagrams800400.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-1-2 col-sm-1-2"><div><img src="img/snapseed800x400.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-1-2 col-sm-1"><div><img src="img/krave800x800.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-1-2 col-sm-1"><div><img src="img/newbelgium800x800.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-2-3 col-sm-1-2"><div><img src="img/nude1600x800.jpg" class="img-responsive"></div></div>
+            <div class="resp-box col-1-3 col-sm-1-2"><div><img src="img/vitaminwater1200x800.jpg" class="img-responsive"></div></div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Responsive</title>
+
+    <!-- Bootstrap core CSS -->
+	<style type="text/css" media="all">  @import url(css/reset.css);  @import url(css/generic.css);  </style> 
+	<style type="text/css" media="print"> @import url(css/print.css) print; </style>
+    <!-- Custom Google Web Font -->
+    <!-- Add custom CSS here -->
+    <link href="css/responsive.css" rel="stylesheet">
+    <link href="css/geral.css" rel="stylesheet">
+</head>
+
+<body>
+	<h1>Colunas</h1>
+	<div>
+    	<div class="resp-box col-1 CABARET col-sm-2"><p><span>col 1</span></p></div>
+        <div class="clear"></div>
+        <div class="resp-box col-2 CASABLANCA"><p><span>col 2</span></p></div>
+        <div class="resp-box col-2 EBONY"><p><span>col 2</span></p></div>
+        <div class="clear"></div>
+        <div class="resp-box col-3 EUCALYPTUS"><p><span>col 3</span></p></div>
+        <div class="resp-box col-3 FLAMINGO"><p><span>col 3</span></p></div>
+        <div class="resp-box col-3 GOLD"><p><span>col 3</span></p></div>
+        <div class="clear"></div>
+        <div class="resp-box col-4 HONEY"><p><span>col 4</span></p></div>
+        <div class="resp-box col-4 REBECCAPURPLE"><p><span>col 4</span></p></div>
+        <div class="resp-box col-4 EUCALYPTUS"><p><span>col 4</span></p></div>
+        <div class="resp-box col-4 FLAMINGO"><p><span>col 4</span></p></div>
+        <div class="clear"></div>
+        <div class="resp-box col-5 CABARET"><p><span>col 5</span></p></div>
+        <div class="resp-box col-5 CASABLANCA"><p><span>col 5</span></p></div>
+        <div class="resp-box col-5 EBONY"><p><span>col 5</span></p></div>
+        <div class="resp-box col-5 WAX"><p><span>col 5</span></p></div>
+        <div class="resp-box col-5 TALL"><p><span>col 5</span></p></div>
+        <div class="clear"></div>
+        <div class="resp-box col-6 REBECCAPURPLE"><p><span>col 6</span></p></div>
+        <div class="resp-box col-6 POMEGRANATE"><p><span>col 6</span></p></div>
+        <div class="resp-box col-6 MEDIUM"><p><span>col 6</span></p></div>
+        <div class="resp-box col-6 MARINO"><p><span>col 6</span></p></div>
+        <div class="resp-box col-6 HONEY"><p><span>col 6</span></p></div>
+        <div class="resp-box col-6 GOLD"><p><span>col 6</span></p></div>
+        <div class="clear"></div>
+        <div class="resp-box col-7 CABARET"><p><span>col 7</span></p></div>
+        <div class="resp-box col-7 CASABLANCA"><p><span>col 7</span></p></div>
+        <div class="resp-box col-7 EBONY"><p><span>col 7</span></p></div>
+        <div class="resp-box col-7 WAX"><p><span>col 7</span></p></div>
+        <div class="resp-box col-7 HONEY"><p><span>col 7</span></p></div>
+        <div class="resp-box col-7 TALL"><p><span>col 7</span></p></div>
+        <div class="resp-box col-7 REBECCAPURPLE"><p><span>col 7</span></p></div>
+        <div class="clear"></div>
+        <div class="resp-box col-8 REBECCAPURPLE"><p><span>col 8</span></p></div>
+        <div class="resp-box col-8 POMEGRANATE"><p><span>col 8</span></p></div>
+        <div class="resp-box col-8 MEDIUM"><p><span>col 8</span></p></div>
+        <div class="resp-box col-8 MARINO"><p><span>col 8</span></p></div>
+        <div class="resp-box col-8 HONEY"><p><span>col 8</span></p></div>
+        <div class="resp-box col-8 GOLD"><p><span>col 8</span></p></div>
+        <div class="resp-box col-8 CASABLANCA"><p><span>col 8</span></p></div>
+        <div class="resp-box col-8 CABARET"><p><span>col 8</span></p></div>
+		<div class="clear"></div>
+        <div class="resp-box col-9 CABARET"><p><span>col 9</span></p></div>
+        <div class="resp-box col-9 CASABLANCA"><p><span>col 9</span></p></div>
+        <div class="resp-box col-9 EBONY"><p><span>col 9</span></p></div>
+        <div class="resp-box col-9 WAX"><p><span>col 9</span></p></div>
+        <div class="resp-box col-9 HONEY"><p><span>col 9</span></p></div>
+        <div class="resp-box col-9 TALL"><p><span>col 9</span></p></div>
+        <div class="resp-box col-9 GOLD"><p><span>col 9</span></p></div>
+        <div class="resp-box col-9 REBECCAPURPLE"><p><span>col 9</span></p></div>
+        <div class="resp-box col-9 MEDIUM"><p><span>col 9</span></p></div>
+		<div class="clear"></div>
+        <div class="resp-box col-10 CABARET"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 CASABLANCA"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 EBONY"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 WAX"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 HONEY"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 TALL"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 GOLD"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 REBECCAPURPLE"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 MEDIUM"><p><span>col 10</span></p></div>
+        <div class="resp-box col-10 WAX"><p><span>col 10</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <h1>Combinando colunas</h1>
+    <h2>3 colunas</h2>
+    <div>
+        <div class="resp-box col-1-3 HONEY"><p><span>col 1 de 3</span></p></div>
+        <div class="resp-box col-1-3 CASABLANCA"><p><span>col 1 de 3</span></p></div>
+        <div class="resp-box col-1-3 MEDIUM"><p><span>col 1 de 3</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <div>
+        <div class="resp-box col-1-3 HONEY"><p><span>col 1 de 3</span></p></div>
+        <div class="resp-box col-2-3 CASABLANCA"><p><span>col 2 de 3</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <h2>4 colunas</h2>
+    <div>
+        <div class="resp-box col-1-4 HONEY"><p><span>col 1 de 4</span></p></div>
+        <div class="resp-box col-1-4 CASABLANCA"><p><span>col 1 de 4</span></p></div>
+        <div class="resp-box col-1-4 HONEY"><p><span>col 1 de 4</span></p></div>
+        <div class="resp-box col-1-4 EUCALYPTUS"><p><span>col 1 de 4</span></p></div>
+        <div class="clear"></div>
+    </div>
+
+    <div>
+        <div class="resp-box col-1-4 HONEY"><p><span>col 1 de 4</span></p></div>
+        <div class="resp-box col-3-4 CASABLANCA"><p><span>col 3 de 4</span></p></div>
+        <div class="clear"></div>
+    </div>
+
+    <div>
+        <div class="resp-box col-1-4 HONEY"><p><span>col 1 de 3</span></p></div>
+        <div class="resp-box col-2-4 CASABLANCA"><p><span>col 2 de 3</span></p></div>
+        <div class="resp-box col-1-4 HONEY"><p><span>col 1 de 3</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <h2>5 colunas</h2>
+    <div>
+        <div class="resp-box col-1-5 CABARET"><p><span>col 1 de 5</span></p></div>
+        <div class="resp-box col-1-5 CASABLANCA"><p><span>col 1 de 5</span></p></div>
+        <div class="resp-box col-1-5 EBONY"><p><span>col 1 de 5</span></p></div>
+        <div class="resp-box col-1-5 EUCALYPTUS"><p><span>col 1 de 5</span></p></div>
+        <div class="resp-box col-1-5 FLAMINGO"><p><span>col 1 de 5</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <div>
+        <div class="resp-box col-2-5 CABARET"><p><span>col 2 de 5</span></p></div>
+        <div class="resp-box col-2-5 CASABLANCA"><p><span>col 2 de 5</span></p></div>
+        <div class="resp-box col-1-5 EBONY"><p><span>col 1 de 5</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <div>
+        <div class="resp-box col-4-5 CABARET"><p><span>col 4 de 5</span></p></div>
+        <div class="resp-box col-1-5 EBONY"><p><span>col 1 de 5</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <h2>6 colunas</h2>
+    <div>
+        <div class="resp-box col-1-6 CABARET"><p><span>col 1 de 6</span></p></div>
+        <div class="resp-box col-1-6 CASABLANCA"><p><span>col 1 de 6</span></p></div>
+        <div class="resp-box col-1-6 EBONY"><p><span>col 1 de 6</span></p></div>
+        <div class="resp-box col-1-6 EUCALYPTUS"><p><span>col 1 de 6</span></p></div>
+        <div class="resp-box col-1-6 FLAMINGO"><p><span>col 1 de 6</span></p></div>
+        <div class="resp-box col-1-6 GOLD"><p><span>col 1 de 6</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <div>
+        <div class="resp-box col-4-6 CABARET"><p><span>col 1 de 6</span></p></div>
+        <div class="resp-box col-1-6 CASABLANCA"><p><span>col 1 de 6</span></p></div>
+        <div class="resp-box col-1-6 EBONY"><p><span>col 1 de 6</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <h2>7 colunas</h2>
+    <div>
+        <div class="resp-box col-4-7 CABARET"><p><span>col 4 de 7</span></p></div>
+        <div class="resp-box col-3-7 CASABLANCA"><p><span>col 3 de 7</span></p></div>
+        <div class="clear"></div>
+    </div>
+        <div>
+        <div class="resp-box col-2-7 CASABLANCA"><p><span>col 2 de 7</span></p></div>
+        <div class="resp-box col-2-7 EUCALYPTUS"><p><span>col 2 de 7</span></p></div>
+        <div class="resp-box col-2-7 FLAMINGO"><p><span>col 2 de 7</span></p></div>
+        <div class="resp-box col-1-7 GOLD"><p><span>col 1 de 7</span></p></div>
+        <div class="clear"></div>
+    </div>
+    
+    <h2>8 colunas</h2>
+
+    <div>
+        <div class="resp-box col-1-8 CASABLANCA"><p><span>col 1 de 8</span></p></div>
+        <div class="resp-box col-1-8 CABARET"><p><span>col 1 de 8</span></p></div>
+        <div class="resp-box col-1-8 EBONY"><p><span>col 1 de 8</span></p></div>
+        <div class="resp-box col-1-8 EUCALYPTUS"><p><span>col 1 de 8</span></p></div>
+        <div class="resp-box col-1-8 FLAMINGO"><p><span>col 1 de 8</span></p></div>
+        <div class="resp-box col-1-8 GOLD"><p><span>col 1 de 8</span></p></div>
+        <div class="resp-box col-1-8 HONEY"><p><span>col 1 de 8</span></p></div>
+        <div class="resp-box col-1-8 MARINO"><p><span>col 1 de 8</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <div>
+        <div class="resp-box col-2-8 CASABLANCA"><p><span>col 2 de 8</span></p></div>
+        <div class="resp-box col-5-8 CABARET"><p><span>col 5 de 8</span></p></div>
+        <div class="resp-box col-1-8 EBONY"><p><span>col 1 de 8</span></p></div>
+        <div class="clear"></div>
+    </div>
+    
+    <h2>9 colunas</h2>
+    <div>
+        <div class="resp-box col-2-9 EBONY"><p><span>col 2 de 9</span></p></div>
+        <div class="resp-box col-5-9 WAX"><p><span>col 5 de 9</span></p></div>
+        <div class="resp-box col-1-9 TALL"><p><span>col 1 de 9</span></p></div>
+        <div class="resp-box col-1-9 REBECCAPURPLE"><p><span>col 1 de 9</span></p></div>
+        <div class="clear"></div>
+    </div>
+    <div>
+        <div class="resp-box col-2-9 EBONY"><p><span>col 2 de 9</span></p></div>
+        <div class="resp-box col-3-9 WAX"><p><span>col 3 de 9</span></p></div>
+        <div class="resp-box col-4-9 TALL"><p><span>col 4 de 9</span></p></div>
+        <div class="clear"></div>
+    </div>
+    
+    <h1>Media queries</h1>
+    <div>
+        <div class="resp-box EBONY col-1 col-xs-1-3 col-sm-1-2 col-md-1 col-lg-1-3 col-xl-1"><p><span>col-1<br>col-xs-1-3<br>col-sm-1-2<br>col-md-1<br>col-lg-1-3<br>col-xl-1</span></p></div>
+        <div class="resp-box WAX col-1 col-xs-1-3 col-sm-1-2 col-md-1-2 col-lg-1-3 col-xl-1"><p><span>col-1<br>col-xs-1-3<br>col-sm-1-2<br>col-md-1-2<br>col-lg-1-3<br>col-xl-1</span></p></div>
+        <div class="resp-box TALL col-1 col-xs-1-3 col-sm-1 col-md-1-2 col-lg-1-3 col-xl-1"><p><span>col-1<br>col-xs-1-3<br>col-sm-1<br>col-md-1-2<br>col-lg-1-3<br>col-xl-1</span></p></div>
+        <div class="clear"></div>
+    </div>
+    
+    <div id="wrapper-center" class="">
+    	<div>
+        	<div class="resp-box EBONY col-1-4 col-md-1-2 col-sm-1-2 col-xs-1"><p><span>col-1-4<br>col-md-1-2<br>col-sm-1-2<br>col-xs-1</span></p></div>
+            <div class="resp-box CASABLANCA col-1-4 col-md-1-2 col-sm-1-2 col-xs-1"><p><span>col-1-4<br>col-md-1-2<br> col-sm-1-2<br>col-xs-1</span></p></div>
+            <div class="resp-box EUCALYPTUS col-1-4 col-md-1-2 col-sm-1-2 col-xs-1"><p><span>col-1-4<br>col-md-1-2<br> col-sm-1-2<br>col-xs-1</span></p></div>
+            <div class="resp-box GOLD col-1-4 col-md-1-2 col-sm-1-2 col-xs-1"><p><span>col-1-4<br>col-md-1-2<br>col-sm-1-2<br>col-xs-1</span></p></div>
+            <div class="clear"></div>
+        </div>
+    </div>
+    <div id="wrapper-center" class="">
+    	<div>
+        	<div class="resp-box EBONY col-1-4 col-md-1-2 col-sm-1-2 col-xs-1"><p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tortor justo</span></p></div>
+            <div class="resp-box CASABLANCA col-1-4 col-md-1-2 col-sm-1-2 col-xs-1"><p><span>Etiam quam lectus, vestibulum ac velit at, efficitur viverra nisl. Fusce accumsan varius eleifend. </span></p></div>
+            <div class="resp-box EUCALYPTUS col-1-4 col-md-1-2 col-sm-1-2 col-xs-1"><p><span>Fusce ut orci eros. Cras facilisis consequat odio, id facilisis elit elementum a. Fusce vestibulum, mauris et tempor tempus, leo nisi rhoncus velit, at auctor ligula mauris nec leo. Donec vulputate ex sed dolor rhoncus elementum. Donec porttitor nisi eu maximus elementum. Duis tempor blandit lorem, in aliquet mauris ullamcorper a. Aliquam fermentum scelerisque lorem ut blandit. Ut porta euismod sem sit amet consequat. Fusce congue leo ligula. Nullam porttitor pellentesque ipsum ut scelerisque. Praesent ornare, libero at volutpat finibus, lacus dolor scelerisque orci, non efficitur magna elit dignissim massa.</span></p></div>
+            <div class="resp-box GOLD col-1-4 col-md-1-2 col-sm-1-2 col-xs-1"><p><span>col-1-4<br>col-md-1-2<br>col-sm-1-2<br>col-xs-1</span></p></div>
+            <div class="clear"></div>
+        </div>
+    </div>
+    <div id="wrapper-center" class="">
+    	<div>
+        	<div class="resp-box EBONY col-xl-hide col-lg-2-4 col-md-1 col-sm-1-3 col-xs-1"><p><span>col-xl-hide<br>col-lg-2-4<br>col-md-1<br>col-sm-1-3<br>col-xs-1</span></p></div>
+            <div class="resp-box CASABLANCA col-xl-2-4 col-lg-hide col-md-1 col-sm-1-3 col-xs-hide"><p><span>col-xl-2-4<br>col-lg-hide<br>col-md-1<br>col-sm-1-3<br>col-xs-hide</span></p></div>
+            <div class="resp-box EUCALYPTUS col-xl-1-4 col-lg-1-4 col-md-hide col-sm-1-3 col-xs-hide"><p><span>col-xl-1-4<br>col-lg-1-4<br>col-md-hide<br>col-sm-1-3<br>col-xs-hide</span></p></div>
+            <div class="resp-box GOLD col-xl-1-4 col-lg-1-4 col-md-1 col-sm-hide col-xs-1"><p><span>col-xl-1-4<br>col-lg-1-4<br>col-md-1<br>col-sm-hide<br>col-xs-1</span></p></div>
+            <div class="resp-box EUCALYPTUS col-xs-show col-xs-1" id="conteudo-oculto"><p><span>Este conteúdo somente aparecerá em telas menores que 586px</span></p></div>
+            <div class="resp-box CASABLANCA col-sm-show col-sm-1" id="conteudo-oculto"><p><span>Este conteúdo somente aparecerá em telas > 568px e < 768</span></p></div>
+            <div class="resp-box EBONY col-md-show col-md-1" id="conteudo-oculto"><p><span>Este conteúdo somente aparecerá em telas > 768px e < 1024px</span></p></div>
+            <div class="resp-box GOLD col-lg-show col-lg-1" id="conteudo-oculto"><p><span>Este conteúdo somente aparecerá em telas > 1024px e < 1280px</span></p></div>
+            <div class="resp-box WAX col-xl-show col-xl-1" id="conteudo-oculto"><p><span>Este conteúdo somente aparecerá em telas ≥ 1280px</span></p></div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</body>
+</html>
